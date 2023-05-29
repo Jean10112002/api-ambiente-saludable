9a3ed87baeafef2f06fa09f72c283ef0bc6d3b82
Janblack07 <jandryjacob20@gmail.com>
2023-05-29 18:15:33 -0500

    Se realizo la instalacion de cloudinary con exito

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Imagen;
 use Illuminate\Http\Request;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
 class ImagenController extends Controller
 {
