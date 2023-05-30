2c08b6192a9bf8d8b2ece17322efb664acdad382
Jean Rodriguez <jeanpierrerodriguezzambrano@gmail.com>
2023-05-30 00:50:03 -0500

    auth lista con seeder para jurado y admin que falta llenar con los datos reales

--- app/Http/Controllers/JuradoController.php
+++ app/Http/Controllers/JuradoController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Jurado;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 class JuradoController extends Controller
 {
     /**
@@ -12,6 +14,16 @@ class JuradoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    private  $rulesLogin = array(
+        'email' => 'required|email',
+        'password' => 'required'
+    );
+    private $messagesLogin = array(
+        'email.unique' => 'ya existe ese email.',
+        'email.required' => 'email es requerido.',
+        'email.email' => 'debe ser un email correcto.',
+        'password.required' => 'debe ingresar una password',
+    );
     public function index()
     {
         //
@@ -60,5 +72,60 @@ class JuradoController extends Controller
     public function destroy(Jurado $jurado)
     {
         //
+    }
+
+
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), $this->rulesLogin, $this->messagesLogin);
+        if ($validator->fails()) {
+            $messages = $validator->messages();
+            return response()->json(["messages" => $messages], 500);
+        }
+
+         $user = Jurado::where("email", "=", $request->email)->first();
+        if (isset($user->id)) {
+            if (Hash::check($request->password, $user->password)) {
+                //creamos el token
+                $token = $user->createToken("auth_token")->plainTextToken;
+                //si está todo ok
+                return response()->json([
+
+                    "messages" => "¡Usuario logueado exitosamente!",
+                    "access_token" => $token
+                ],200);
+            }  else {
+            return response()->json([
+                "status" => 0,
+                "messages" => "credenciales incorrectas",
+            ], 500);
+        }
+    }else{
+        return response()->json([
+            "status" => 0,
+            "messages" => "Usuario no registrado",
+        ], 404);
+    }
+}
+
+    public function userProfile()
+    {
+        $usuario=auth()->user();
+        return response()->json([
+            "msg" => "Acerca del perfil de usuario",
+            "user" => $usuario,
+            /* "usuario"=>$user, */
+        ]);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+            "status" => 1,
+            "messages" => "Cierre de Sesión",
+        ]);
     }
 }
--- database/seeders/Jurado.php
+++ database/seeders/Jurado.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Jurado as ModelsJurado;
 use Illuminate\Database\Seeder;
 
 class Jurado extends Seeder
@@ -14,6 +14,39 @@ class Jurado extends Seeder
      */
     public function run()
     {
-        //
+        /* ModelsJurado::create([
+            "nombre"=>"",
+            "apellido"=>"",
+            "cedula"=>"",
+            "email"=>"",
+            "password"=>bcrypt('')
+        ]);
+        ModelsJurado::create([
+            "nombre"=>"",
+            "apellido"=>"",
+            "cedula"=>"",
+            "email"=>"",
+            "password"=>bcrypt('')
+        ]);
+        ModelsJurado::create([
+            "nombre"=>"",
+            "apellido"=>"",
+            "cedula"=>"",
+            "email"=>"",
+            "password"=>bcrypt('')
+        ]);
+        ModelsJurado::create([
+            "nombre"=>"",
+            "apellido"=>"",
+            "cedula"=>"",
+            "email"=>"",
+            "password"=>bcrypt('')
+        ]); */
+        ModelsJurado::create([
+            "nombre"=>"admin",
+            "apellido"=>"admin",
+            "cedula"=>"1313626440",
+            "email"=>"admin@hotmail.com",
+            "password"=>bcrypt('admin123')]);
     }
 }
