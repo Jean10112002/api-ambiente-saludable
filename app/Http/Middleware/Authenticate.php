c3f5211d8cc5b2723c6924a32e9533f0638d6028
Jean Rodriguez <jeanpierrerodriguezzambrano@gmail.com>
2023-05-30 08:39:24 -0500

    mostrar error de no aunteticado solucionado para la autenticacion

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,15 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Http\Response;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+
+    public function handle($request, Closure $next, ...$guards){
+        return response()->json(['error' => 'No estas autenticado.'], 401);
     }
 }
