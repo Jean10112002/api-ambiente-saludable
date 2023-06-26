a92a01cc612b6bf69b9554a773ba1f99bd1231dc
Jean Rodriguez <jeanpierrerodriguezzambrano@gmail.com>
2023-06-26 16:58:34 -0500

    nuevo jurado agregado

--- database/seeders/Jurado.php
+++ database/seeders/Jurado.php
@@ -37,7 +37,8 @@ class Jurado extends Seeder
     $jurado3=new Persona('Julia Angelita','Cordero Guillén','0999142496','juliaangelita991424@hotmail.com','0xIfnBdN^1GA15');
     $jurado4=new Persona('José Gabriel','Viteri Espinoza','0996398175','josegrabriel_963981@hotmail.com','Ac&9hF^2Zom^0q');
     $jurado5=new Persona('Ana Elizabeth','Ágreda De la Paz','0989560242','anaelizabeth_895602@hotmail.com','nIV1P0%p1h0L^T');
-        $jurados = array($jurado1,$jurado2,$jurado3,$jurado4,$jurado5);
+    $jurado6=new Persona('Juan Fernando','Pesántes Muñoz','0992365180','juanfernando_923651@hotmail.com','hi1SrMWR*M7ZQ2');
+        $jurados = array($jurado1,$jurado2,$jurado3,$jurado4,$jurado5,$jurado6);
         ModelsJurado::create([
             "nombre" => "admin",
             "apellido" => "admin",
